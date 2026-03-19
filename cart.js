1085b586c3c413f758dde8408b68fa884e28aa7c
Aybi22 <your_email_address@example.com>
2026-03-19 20:58:45 +0000

    "Refine and structure filter section on  mobile screens"

--- cart.js
+++ cart.js
@@ -1,78 +1,33 @@
-
-import  detail from'./details.js';
-import products from'./products.js'
+import detail from "./details.js";
+import products from "./products.js";
 
 detail();
 
+export function showCart() {
+  let cart = document.querySelector(".cart");
+  let overlay = document.querySelector(".overlay");
 
+  if (cart.className === "cart") {
+    cart.classList.add("modal-box");
 
+    overlay.classList.add("overlay-box");
+  } else {
+    cart.classList.remove("modal-box");
+    overlay.classList.remove("overlay-box");
+  }
+}
 
+export function closeOver() {
+  let cart = document.querySelector(".cart");
+  let overlay = document.querySelector(".overlay");
+  overlay.classList.remove("overlay-box");
+  cart.classList.remove("modal-box");
+}
 
-export function showCart(){
-    
+export function closeModal() {
+  let cart = document.querySelector(".cart");
+  let overlay = document.querySelector(".overlay");
 
-    let cart = document.querySelector('.cart');
-    let overlay = document.querySelector('.overlay');
-
-    if (cart.className === "cart") {
-        cart.classList.add("modal-box");
-        
-        overlay.classList.add("overlay-box");
-
-
-    } else {
-        cart.classList.remove("modal-box");
-        overlay.classList.remove("overlay-box");
-    }
-
- }
-
-
-
-  export function closeOver() {
-    let cart = document.querySelector('.cart');
-    let overlay = document.querySelector('.overlay');
-        overlay.classList.remove('overlay-box');
-        cart.classList.remove('modal-box');
-    }
-
-
-
-    
-
-export function closeModal(){
-    let cart = document.querySelector('.cart');
-    let overlay = document.querySelector('.overlay');
-    
-        cart.classList.remove("modal-box");
-        overlay.classList.remove("overlay-box");
-     
- }
- 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
- 
-    
+  cart.classList.remove("modal-box");
+  overlay.classList.remove("overlay-box");
+}
